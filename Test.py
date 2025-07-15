2dcf4b5fb47451a567be6742f51eee08f0e4a2f5
Noah Kling <noahkling@Noahs-MacBook-Air.local>
2025-07-15 21:40:13 +0200

    Finished intermediate project

--- Test.py
+++ Test.py
@@ -1,6 +1,32 @@
-from cryptography.fernet import Fernet
+import random
 
+ROWS=3
+COLUMNS=3
 
-key = Fernet.generate_key()
+SYMBOLS={
+    "A":4,
+    "B":8,
+    "C":12,
+    "D":16
+}
 
-print(key)
+def get_Slot_Machine_Spin(rows,columns,symbols):
+    all_symbols = []
+    for symbol, symbol_count in symbols.items():
+        temp = [symbol]*symbol_count
+        all_symbols = all_symbols + temp
+    
+    column = []
+    temp=[]
+
+    for i in range(columns*rows):
+        value = random.choice(all_symbols)
+        temp.append(value)
+        if (i+1) % 3 == 0:
+            column.append(temp)
+            temp=[]
+    
+    return column
+
+print(get_Slot_Machine_Spin(ROWS,COLUMNS,SYMBOLS))
+
